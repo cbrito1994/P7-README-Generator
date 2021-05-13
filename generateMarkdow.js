9d6996f1a9d3a9f474f36007c5e45d46a3e6f0ae
Carlos Brito <cbrito1994@gmail.com>
2021-05-12 21:09:13 -0500

    Added the function to render the link to the license info

--- generateMarkdow.js
+++ generateMarkdow.js
@@ -1,19 +1,50 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+    let licenseNoSpaces = license[0].split(' ').join('');
+    return `https://img.shields.io/static/v1?label=license&message=${licenseNoSpaces}&color=brightgreen`
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
+function renderLicenseLink(license) {
+    let response;
+    if(license){
+        switch(license[0]){
+            case 'MIT License':
+                response = 'https://choosealicense.com/licenses/mit/';
+                break;
+            case 'GNU AGPLv3':
+                response = 'https://choosealicense.com/licenses/agpl-3.0/';
+                break;
+            case 'Mozilla Public License 2.0':
+                response = 'https://choosealicense.com/licenses/mpl-2.0/';
+                break;
+            case 'Apache License 2.0':
+                response = 'https://choosealicense.com/licenses/apache-2.0/';
+                break;
+            case 'Boost Software License 1.0':
+                response = 'https://choosealicense.com/licenses/bsl-1.0/';
+                break
+            case 'The Unlicense':
+                response = 'https://choosealicense.com/licenses/unlicense/';
+                break
+            default:
+                response = 'No link found';
+        }
+        return response;
+    }
+}
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection (license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-    
+const generateMarkdown = (data) => {
+
     return `# ${data.title.toUpperCase()}
+${renderLicenseBadge(data.license)}
+
 ## Description
 ${data.description}
 
@@ -38,7 +69,8 @@ ${data.contribution}
 ${data.tests}
 
 ## License
-${data.license}
+${data.license}\n
+Link to the license info: ${renderLicenseLink(data.license)}
 
 ## Questions
 Github profile: ${data.github}\n
