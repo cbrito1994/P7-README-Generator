2645b1c44bd6bf1a9f93a6196b989bd909bf89fd
Carlos Brito <cbrito1994@gmail.com>
2021-05-12 21:27:11 -0500

    Made corrections to the generateMarkdown js file

--- generateMarkdow.js
+++ generateMarkdow.js
@@ -33,15 +33,14 @@ function renderLicenseLink(license) {
                 response = 'No link found';
         }
         return response;
+    } else {
+        response = '';
     }
+    return response;
 }
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection (license) {}
 
 // TODO: Create a function to generate markdown for README
 const generateMarkdown = (data) => {
-
     return `# ${data.title.toUpperCase()}
 ![license](${renderLicenseBadge(data.license)})
 
