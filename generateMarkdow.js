94a612a02d37a865ce87f0ddb64fb2c0e6dc7759
Carlos Brito <cbrito1994@gmail.com>
2021-05-12 19:37:17 -0500

    Created the structure for the md file

--- generateMarkdow.js
+++ generateMarkdow.js
@@ -13,7 +13,36 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+    ## Description
+    ${data.description}
 
+    ## Table of Contents
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [Contribution](#contribution)
+    - [Tests](#tests)
+    - [License](#license)
+    - [Questions](#questions)
+
+    ## Installation
+    ${data.installation}
+
+    ## Usage
+    ${data.usage}
+
+    ## How to Contribute
+    ${data.contribution}
+
+    ## Tests
+    ${data.tests}
+
+    ## License
+    ${data.license}
+
+    ##Questions
+    Github profile: ${data.github}
+    Email address: ${data.email}
+    Instructions to reach me by email: ${data.howToReachEmail}
 `;
 }
 
